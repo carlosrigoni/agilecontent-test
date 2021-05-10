a97ce35fdc3a75de0765cb796ada39d7e04130d0
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2021-05-10 01:49:27 -0300

    detalhes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 
 import GlobalStyle from '../styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
@@ -8,20 +7,10 @@ import dark from '../styles/themes/dark';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <ThemeProvider theme={dark}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={dark}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
